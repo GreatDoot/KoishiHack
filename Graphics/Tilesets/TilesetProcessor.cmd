f3b38be6d5fdf726611bcd03573d6127acff3d76
Ditto <myriadarrows0@gmail.com>
2024-11-22 16:22:37 +0000

    attempt at map insertion (PLIST repointed)

--- Graphics/Tilesets/TilesetProcessor.cmd
+++ Graphics/Tilesets/TilesetProcessor.cmd
@@ -8,23 +8,11 @@ for /f "tokens=*" %%m in (png.txt) do (
 
     echo %%m
 
-    ..\..\..\EventAssembler\Tools\Png2Dmp.exe %%m --lz77 -o %%~nm"-graphics.dmp" -po %%~nm"-palette.dmp"
+    ..\..\..\EventAssembler\Tools\Png2Dmp.exe %%m --lz77 -o ..\dmp\%%~nm"-graphics.dmp" -po ..\palettes\input\%%~nm"-palette.dmp"
 
 )
 
 @del png.txt
-
-@dir *.dmp /b > dmp.txt
-
-for /f "tokens=*" %%m in (dmp.txt) do (
-    
-    echo %%m
-
-    move %%m "..\dmp\"%%m
-
-)
-
-@del dmp.txt
 
 echo PNGs processed!
 
@@ -36,23 +24,11 @@ for /f "tokens=*" %%m in (mapchip_config.txt) do (
 
     echo %%m
 
-    ..\..\..\Tools\gbalz77tool.exe -o %%~nm"-mapchip.dmp" %%m compress
+    ..\..\..\Tools\gbalz77tool.exe -o ..\dmp\%%~nm"-mapchip.dmp" %%m compress
 
 )
 
 @del mapchip_config.txt
-
-@dir *.dmp /b > dmp.txt
-
-for /f "tokens=*" %%m in (dmp.txt) do (
-    
-    echo %%m
-
-    move %%m "..\dmp\"%%m
-
-)
-
-@del dmp.txt
 
 echo MAPCHIPs processed!
 
