1734c2a9c7914982e683934481cc49d557ce1687
GreatDoot <119607063+GreatDoot@users.noreply.github.com>
2024-12-16 16:31:45 +0000

    WAITER WAITER MORE INSANITY PLEASE

---
+++ Maps/chapter1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Fe8_Castle_Improved" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="Fe8_Castle_Improved.png" width="512" height="512"/>
+</tileset>
---
+++ Maps/chapter2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Fe8_Castle_Improved" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../Graphics/Tilesets/png/Fe8_Castle_Improved.png" width="256" height="256"/>
+</tileset>
