15653f8646f769688cbf5704447d3cf3a5b8a3d7
Ditto <myriadarrows0@gmail.com>
2024-12-16 18:44:33 +0000

    fix some butchered tiled configs

--- Maps/Fe8_Castle_Improved.tsx
+++ Maps/Fe8_Castle_Improved.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Fe8_Castle_Improved" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../Graphics/Tilesets/png/Fe8_Castle_Improved.png" width="512" height="512"/>
+<tileset version="1.10" tiledversion="1.11.0" name="Fe8_Castle_Improved" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <image source="Fe8_Castle_Improved.png" width="512" height="512"/>
 </tileset>
